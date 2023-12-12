3c66ecfc7cb18af6ad4aa446281a3fbb647fa796
Filip Czapliński <filip.czaplinski92@gmail.com>
2023-12-12 17:00:34 +0100

    Add Phrase entity

---
+++ src/Entity/Phrase.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PhraseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PhraseRepository::class)]
+class Phrase
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $phrase = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $translate = null;
+
+    #[ORM\Column]
+    private ?bool $isUsed = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPhrase(): ?string
+    {
+        return $this->phrase;
+    }
+
+    public function setPhrase(string $phrase): static
+    {
+        $this->phrase = $phrase;
+
+        return $this;
+    }
+
+    public function getTranslate(): ?string
+    {
+        return $this->translate;
+    }
+
+    public function setTranslate(string $translate): static
+    {
+        $this->translate = $translate;
+
+        return $this;
+    }
+
+    public function isIsUsed(): ?bool
+    {
+        return $this->isUsed;
+    }
+
+    public function setIsUsed(bool $isUsed): static
+    {
+        $this->isUsed = $isUsed;
+
+        return $this;
+    }
+}
