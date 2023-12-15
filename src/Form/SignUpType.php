c561fab2325b68e1681fb225ce576ea53d59748f
Filip Czapliński <filip.czaplinski92@gmail.com>
2023-12-15 14:39:22 +0100

    Email type field on front

--- src/Form/SignUpType.php
+++ src/Form/SignUpType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,7 +14,7 @@ class SignUpType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class)
             ->add('submit', SubmitType::class)
         ;
     }
