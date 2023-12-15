dea2b019e255fd318a02f577fa91c58b460fcea5
Filip Czapliński <filip.czaplinski92@gmail.com>
2023-12-15 17:25:11 +0100

    Admin password protection

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -89,7 +89,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @see PasswordAuthenticatedUserInterface
      */
-    public function getPassword(): string
+    public function getPassword(): ?string
     {
         return $this->password;
     }
