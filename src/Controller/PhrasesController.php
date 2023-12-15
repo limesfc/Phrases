39ab72b6d9afc1dbd0074c0fa27be43f1f7286aa
Filip Czapliński <filip.czaplinski92@gmail.com>
2023-12-15 14:49:49 +0100

    Save new user by form

--- src/Controller/PhrasesController.php
+++ src/Controller/PhrasesController.php
@@ -8,13 +8,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PhrasesController extends AbstractController
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    ) {
+    }
+
     #[Route('/', name: 'phrases_signup', methods: ['GET', 'POST'])]
     public function signup(Request $request): Response
     {
-        $form = $this->createForm(SignUpType::class, new User());
+        $user = new User();
+        $form = $this->createForm(SignUpType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
 
         return $this->render('phrases/index.html.twig', [
             'sign_up_form' => $form,
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
@@ -16,6 +17,8 @@ class User
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Email]
     private ?string $email = null;
 
     #[ORM\Column]
