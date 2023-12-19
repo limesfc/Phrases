3e0e509cc99412f60c0f53c0e757210a934b9013
Filip Czapliński <filip.czaplinski92@gmail.com>
2023-12-19 15:41:48 +0100

    Some functional test for sign up controller

--- tests/Functional/PhrasesControllerTest.php
+++ tests/Functional/PhrasesControllerTest.php
@@ -2,16 +2,52 @@
 
 namespace App\Tests\Functional;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class PhrasesControllerTest extends WebTestCase
 {
+    private EntityManager $entityManager;
+
+    private KernelBrowser $client;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+
+        $this->entityManager = $this->client->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
     public function testRenderSignUpLandingPage(): void
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $this->client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Sign Up for learn English phrases');
+    }
+
+    public function testSignUpForm()
+    {
+        $countOfUsers = $this->entityManager
+            ->getRepository(User::class)
+            ->count([]);
+
+        $this->client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+
+        $this->client->submitForm('sign_up_submit', [
+            'sign_up[email]' => 'test@form.com',
+        ]);
+
+        $this->assertResponseRedirects();
+        $this->client->followRedirect();
+        $this->assertSelectorTextContains('h1', 'Sign Up for learn English phrases');
+        $this->assertSelectorNotExists('li:contains("This value is already used.")');
+        $this->assertEquals($countOfUsers + 1, $this->entityManager->getRepository(User::class)->count([]));
     }
 }
