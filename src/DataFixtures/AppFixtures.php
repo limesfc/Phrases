4dc3347f0a475123e71d4e7412c702c17d9934c7
Filip Czapliński <filip.czaplinski92@gmail.com>
2023-12-19 16:54:03 +0100

    Add 2 test users to fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private PasswordHasherFactoryInterface $passwordHasherFactory,
+    ) {
+
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $admin = new User();
+        $admin->setEmail('admin@phrases.com');
+        $admin->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+        $admin->setPassword($this->passwordHasherFactory->getPasswordHasher(User::class)->hash('secret123'));
+        $admin->setIsActive(true);
+        $manager->persist($admin);
+
+        $user = new User();
+        $user->setEmail('user@test.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setIsActive(true);
+        $manager->persist($user);
 
         $manager->flush();
     }
